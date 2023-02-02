aa1b89632540a8700969543bb5edaade3b9a4bac
MarcosVAM <viniciusmarcostgtt@gmail.com>
2023-02-02 11:54:43 -0300

    grafico

--- teste/core/views.py
+++ teste/core/views.py
@@ -3,15 +3,34 @@ from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from . import models
 import pandas as pd
+from django.views.decorators.csrf import csrf_exempt
 
+# def grafico(request):
+#     valores = {}
+#     aux = 0
+#     for i in range(27):
+#         for mes in range(12):
+#             df = pd.read_csv(f'C:/Users/marco/Downloads/EstadosFinal/estado{i+1}/hipertensao_diabetes/mes'+str(mes+1)+'.csv', encoding="mbcs")
+#             df2 = df.max()
+#             df3 = df2.max()
+#             dff = df3[-2::]
+#             valores[aux] = dff
+#             aux = aux+1
+#     pacote = {"chave": valores}
+#     return render(request, 'grafico.html',pacote)
 
 def grafico(request):
-    valores = {}
-    for i in range(12):
-        df = pd.read_csv(f'mes{i+1}.csv', encoding="mbcs")
-        df2 = df.max()
-        df3 = df2.max()
-        dff = df3[-2::]
-        valores[i] = dff
-    pacote = {"chave": valores}
+    pacote = {}
+    vetor = []
+    a = 0
+    for i in range(27):
+        for mes in range(12):
+            df = pd.read_csv(f'C:/Users/marco/Documents/GitHub/susana-bot/teste/EstadosFinal/estado{i+1}/hipertensao_diabetes/mes'+str(mes+1)+'.csv', encoding="mbcs") 
+            df2 = df.max()
+            df3 = df2.max()
+            dff = df3[-2::]
+            vetor.append (dff)
+            a  += 1
+    pacote = {"chave": vetor}
+    print(pacote)
     return render(request, 'grafico.html', pacote)
